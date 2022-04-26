f28b76bf3eaf309d6f04a646b459790621cc7f26
Caleb Ockwig <31769782+CJOckwig@users.noreply.github.com>
2022-04-26 00:00:56 -0500

    Add files via upload

--- hospital_DBMS.sql
+++ hospital_DBMS.sql
@@ -2,6 +2,7 @@
 use hospital;
 SET SQL_SAFE_UPDATES = 0;
 
+/*
  drop table patient;
  drop table chart;
  drop table doctor;
@@ -10,14 +11,9 @@ SET SQL_SAFE_UPDATES = 0;
  drop table room;
  drop table hospital_wing;
  drop table visits;
- drop table clerk_wing;
- drop table works;
- drop table wing_room;
- drop table stays_in;
- drop table records;
  drop table treats;
  drop table patient_Chart;
-
+*/
 -- Schemas
  create table patient(patient_ID varchar(10), first_name varchar(30), last_name varchar(30), room_number varchar (9),wing_number varchar(5), primary key(patient_ID));
  create table chart(chart_id varchar(10), patient_ID varchar(10), age varchar (8), allergies varchar(100), medical_history varchar(100), medications varchar(40), family_illnesses varchar(40), primary key(chart_id));
@@ -28,17 +24,10 @@ SET SQL_SAFE_UPDATES = 0;
  create table hospital_wing(wing_number varchar(5),wing_name varchar(20), care_type varchar(20), primary key(wing_number));
  create table visits (patient_ID varchar(10), admit_date varchar(9), admit_reason varchar(50), depart_date varchar(9), symptoms varchar (100), primary key (admit_date, patient_id));
 
--- Relations 
- create table clerk_wing(wing_number varchar(5), staff_id varchar(10));
- create table works(staff_id varchar(10), wing_number varchar(5));
- create table wing_room(wing_number varchar(5), room_number varchar(10));
- create table stays_in( patient_ID varchar(10), room_number varchar(10));
- create table records (admit_date varchar(9), patient_ID varchar(10));
- create table treats (staff_id varchar(10), patient_ID varchar(10));
- create table patient_Chart(chart_id varchar(10), patient_ID varchar(10));
-
-
--- create table patient(patient_ID varchar(10), first_name varchar(30), last_name varchar(30), room_number varchar (9),wing_number varchar(5), primary key(patient_ID));
+-- Relations
+create table treats (staff_id varchar(10), patient_ID varchar(10), primary key(patient_id));
+create table patient_Chart(chart_id varchar(10), patient_ID varchar(10));
+ 
 insert into patient values("p-1", "Steven","Strange", "134","1");
 insert into patient values("p-2", "Bruce","Banner", "156","1");
 insert into patient values("p-3", "Peter","Parker", "079","3");
@@ -55,7 +44,7 @@ insert into patient values("p-11", "Clark","Kent", "233", "4");
 -- (chart_id varchar(10), patient_ID varchar(10), 
 -- age varchar (8), symptoms varchar (30), allergies varchar(30), 
 -- medical_history varchar(40), medications varchar(40), family_illnesses varchar(40));
-insert into chart values("c-1", "p-1", "43", "NO ALLERGIES", "NO SIGNIFICANT HISTORY", "NO MEDICATION", "NO FAMILY ILLNESS");
+insert into chart values("c-1", "p-1", "43", "NO ALLERGIES", "NO SIGNIFICANT HISTORY", "NO MEDICATION", "Diabetes(P)");
 insert into chart values("c-2", "p-2", "39",  "NO ALLERGIES", "Gamma Exposure in 2012", "NO MEDICATION", "NO FAMILY ILLNESS");
 insert into chart values("c-3", "p-3", "21", "ALLERGIC TO SPIDERS AND PENICLLIN", "Bit by radioactive spider in 2017", "NO MEDICATION", "PARENTS DIED IN CAR ACCIDENT");
 insert into chart values("c-4", "p-4", "29", "ALLERGIC TO CATS", "NO SIGNIFICANT HISTORY", "20 mg lexapro", "NO FAMILY ILLNESS");
@@ -108,17 +97,24 @@ insert into hospital_wing values ("4", "West", "urgent care");
 
 -- create table visits (patient_ID varchar(10), admit_date varchar(9), admit_reason varchar(50), depart_date varchar(9), symptoms varchar (100), primary key (admit_date, patient_id));
 insert into visits values("p-1", "4/24/22", "Car Crash, broken arms, Concussion","", "cough, broken arms, dazed and confused");
-insert into visits values("p-2", "4/24/22", "Extreme muscle cramping","", "extreme fatigue, muscle pain, overall 'green'");
-insert into visits values("p-3", "4/24/22", "Coughing up black mucus", "", "cough, fever, headache");
-insert into visits values("p-4", "4/24/22", "anaphylectic shock caused by cat allergy", "", "Fever, Rash");
+insert into visits values("p-2", "4/17/22", "Extreme muscle cramping","", "extreme fatigue, muscle pain, overall 'green'");
+insert into visits values("p-3", "4/23/22", "Coughing up black mucus", "", "cough, fever, headache");
+insert into visits values("p-4", "4/22/22", "anaphylectic shock caused by cat allergy", "", "Fever, Rash");
 insert into visits values("p-5", "4/24/22", "finger arthritis", "", "stiff joints");
-insert into visits values("p-6", "4/24/22", "Cancer", "", "Fever, headache, cough" );
-insert into visits values("p-7", "4/24/22", "'jittery', heart palpitation's, seizures", "","Severe Fatigue, Cough");
-insert into visits values("p-8", "4/24/22", "Insomnia","", "Insomnia, Fever, headache" );
+insert into visits values("p-6", "4/21/22", "Cancer", "", "Fever, headache, cough" );
+insert into visits values("p-7", "4/25/22", "'jittery', heart palpitation's, seizures", "","Severe Fatigue, Cough");
+insert into visits values("p-8", "4/22/22", "Insomnia","", "Insomnia, Fever, headache" );
 insert into visits values("p-9", "4/24/22", "Dementia", "", "Confusion, arthritis, mild cough");
-insert into visits values("p-10","4/24/22", "Heart Palpitations, extreme fatigue", "", "Heartburn, cough, heart palpitations");
+insert into visits values("p-10","4/22/22", "Heart Palpitations, extreme fatigue", "", "Heartburn, cough, heart palpitations");
 insert into visits values("p-11", "4/24/22", "Heart Palpitations, extreme fatigue", "","Heavy Chest, difficulty breathing, red eyes");
 
+-- create table clerk(staff_id varchar(10), name varchar (30), address varchar (40), wing_number varchar(5), primary key(staff_id));
+insert into clerk values("S-C-1", "Peter Venkman", "1771 Groundhog Ave, MN", "1");
+insert into clerk values("S-C-2", "Ray Stantz", "1978 Blues Rd, MN", "2");
+insert into clerk values("S-C-3", "Egon Spengler", "8181 Stripes Rd, MN", "4");
+insert into clerk values("S-C-4", "Winston Zeddemore", "1234 Twinkie St, MN", "3");
 
-
+Select first_name, last_name, age
+From chart natural join patient
+Where patient_id = 'p-1'
 
